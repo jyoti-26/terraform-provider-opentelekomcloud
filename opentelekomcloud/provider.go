ca4da74f5ba486df1b5c2c20433a9415591de0b1
disha-wani <disha.wani@WSD008>
2018-07-03 18:44:13 +0530

    Merge remote-tracking branch 'remotes/terraform/master' into deh_dev

--- opentelekomcloud/provider.go
+++ opentelekomcloud/provider.go
@@ -169,8 +169,8 @@ func Provider() terraform.ResourceProvider {
 			"opentelekomcloud_vpc_route_ids_v2":          dataSourceVPCRouteIdsV2(),
 			"opentelekomcloud_vpc_subnet_v1":             dataSourceVpcSubnetV1(),
 			"opentelekomcloud_vpc_subnet_ids_v1":         dataSourceVpcSubnetIdsV1(),
-			"opentelekomcloud_deh_host_v1":            dataSourceDEHHostV1(),
-			"opentelekomcloud_deh_server_v1":          dataSourceDEHServersV1(),
+			"opentelekomcloud_deh_host_v1":               dataSourceDEHHostV1(),
+			"opentelekomcloud_deh_server_v1":             dataSourceDEHServersV1(),
 		},
 
 		ResourcesMap: map[string]*schema.Resource{
@@ -220,7 +220,7 @@ func Provider() terraform.ResourceProvider {
 			"opentelekomcloud_vpc_peering_connection_accepter_v2": resourceVpcPeeringConnectionAccepterV2(),
 			"opentelekomcloud_vpc_route_v2":                       resourceVPCRouteV2(),
 			"opentelekomcloud_vpc_subnet_v1":                      resourceVpcSubnetV1(),
-			"opentelekomcloud_deh_host_v1":                     resourceDeHHostV1(),
+			"opentelekomcloud_deh_host_v1":                        resourceDeHHostV1(),
 		},
 
 		ConfigureFunc: configureProvider,
